8d6d2a5f6d678a69cc2f54ffa5705a75468742df
Dozin Jang <108924060+dozine@users.noreply.github.com>
2024-01-25 19:47:21 +0900

    Title: 규칙에 맞는 출력, Time: 123ms, Memory: 26MB, Status: Passed - CodeTree

--- 240125/규칙에 맞는 출력/output-that-matches-the-rule.py
+++ 240125/규칙에 맞는 출력/output-that-matches-the-rule.py
@@ -1,6 +1,5 @@
 n=int(input())
-cnt=n
-for i in range(n):
-    for j in range(i+1):
-        print(n-j+1,end=" ")
+for i in range(n, 0, -1):
+    for k in range(i, n + 1):
+        print(k, end=" ")
     print()
