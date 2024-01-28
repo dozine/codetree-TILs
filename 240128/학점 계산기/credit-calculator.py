d933cce8289ac40511899228bb797c1418940f1d
Dozin Jang <108924060+dozine@users.noreply.github.com>
2024-01-28 16:38:31 +0900

    Title: 학점 계산기, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240128/학점 계산기/credit-calculator.py
@@ -0,0 +1,13 @@
+n=int(input())
+arr = list(map(float,input().split()))
+
+sum_val=sum(arr)
+avg=sum_val/n
+
+print(f'{avg:.1f}')
+if avg >=4.0:
+    print("perfect")
+elif avg>=3.0:
+    print("Good")
+else: 
+    print("Poor")
