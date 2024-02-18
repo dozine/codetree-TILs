883293911667a263216e1c9a05342e0ee47412b7
Dozin Jang <108924060+dozine@users.noreply.github.com>
2024-02-18 14:00:36 +0900

    Title: 함수를 이용한 윤년 판별, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240218/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 240218/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -1,8 +1,10 @@
 y=int(input())
 def is_yoon(y):
-    if (y%4==0) or (y%4==0 and y%100==0 and y%400==0) :
+    if y%4==0:
         return "true" 
-    else:
+    if y%4==0 and y%100==0:
         return "false"
-
+    if y%4==0 and y%100==0 and y%400==0:
+        return "true"
+    return "false"
 print(is_yoon(y))
