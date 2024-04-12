aee038cbc0d0543903b1652fa5435bd8f56f8e37
Dozin Jang <108924060+dozine@users.noreply.github.com>
2024-04-13 02:38:59 +0900

    Title: 좌우로 움직이는 로봇, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240413/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -0,0 +1,30 @@
+max_t=50000
+n,m=tuple(map(int,input().split()))
+pos_a,pos_b=[0]*50000,[0]*50000
+
+time_a=1 
+for _ in range(n):
+    t,d=tuple(input().split())
+    for _ in range(int(t)):
+        pos_a[time_a]=pos_a[time_a-1]+(1 if d=='R' else -1)
+        time_a += 1
+time_b=1 
+for _ in range(m):
+    t,d=tuple(input().split())
+    for _ in range(int(t)):
+        pos_b[time_b]=pos_b[time_b-1]+(1 if d=='R' else -1)
+        time_b += 1
+    
+if time_a < time_b:
+    for i in range(time_a,time_b):
+        pos_a[i] = pos_a[i-1]
+elif time_a > time_b:
+    for i in range(time_b,time_a):
+        pos_b[i] =pos_b[i-1]
+    
+cnt=0
+time_max=max(time_a,time_b)
+for i in range(1, time_max):
+    if pos_a[i] == pos_b[i] and pos_a[i-1] !=pos_b[i-1]:
+        cnt+=1
+print(cnt)
