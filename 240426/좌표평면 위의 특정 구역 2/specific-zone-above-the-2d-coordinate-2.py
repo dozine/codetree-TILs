67ac5ecbc61ea5861606fa2953e734a6104c5d1d
Dozin Jang <108924060+dozine@users.noreply.github.com>
2024-04-26 19:19:44 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240426/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -0,0 +1,22 @@
+import sys
+INT_MAX=sys.maxsize
+n=int(input())
+arr=[
+    tuple(map(int,input().split()))
+    for _ in range(n)
+]
+
+ans=INT_MAX
+
+for i in range(n):
+    min_x,max_x=INT_MAX,1
+    min_y,max_y=INT_MAX,1 
+    for j,(x,y) in enumerate(arr):
+        if j==i:
+            continue
+        min_x=min(min_x,x)
+        max_x=max(max_x,x)
+        min_y=min(min_y,y)
+        max_y=max(max_y,y)
+    ans = min(ans, (max_x - min_x)*(max_y -min_y))    
+print(ans)
