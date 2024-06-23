71ef6f73b37844d1344256864d67515dbeb6587a
Dozin Jang <108924060+dozine@users.noreply.github.com>
2024-06-23 16:43:09 +0900

    Title: Date to Date, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240623/Date to Date/date-to-date.py
@@ -0,0 +1,13 @@
+m1,d1,m2,d2=tuple(map(int,input().split()))
+nums_of_days=[0,31,28,31,30,31,30,31,30,31,30,31,30]
+total_days=0
+
+def num_of_days(m,d):
+    days=[0,31,28,31,30,31,30,31,30,31,30,31,30]
+    total_days=0
+    for i in range(1,m):
+        total_days+=days[i]
+    total_days+=d
+    return total_days
+total_days=num_of_days(m2,d2)-num_of_days(m1,d1)
+print(total_days)
