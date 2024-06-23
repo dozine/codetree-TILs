7f4c82f2bf040b09f1e2ff265e57ca74de4dcf2f
Dozin Jang <108924060+dozine@users.noreply.github.com>
2024-06-23 16:07:14 +0900

    Title: 숫자로 이루어진 사각형, Time: 194ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 240623/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -0,0 +1,13 @@
+n=int(input())
+
+def square(n):
+    cnt=1
+    for _ in range(n):
+        for _ in range(n):
+            print(cnt,end=" ")
+            cnt+=1
+            if cnt==10:
+                cnt=1
+        print()
+
+square(n)
