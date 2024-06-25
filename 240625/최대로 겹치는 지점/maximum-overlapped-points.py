5ae7e8151f5c197879978cd4169b175cbdaef945
Dozin Jang <108924060+dozine@users.noreply.github.com>
2024-06-25 23:31:17 +0900

    Title: 최대로 겹치는 지점, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240625/최대로 겹치는 지점/maximum-overlapped-points.py
+++ 240625/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -3,11 +3,9 @@ segments=[
     tuple(map(int,input().split()))
     for _ in range(n)
 ]
-offset=100
 blocks=[0]*200
 
 for x1,x2 in segments:
-    x1,x2=x1+offset,x2+offset
     for i in range(x1,x2+1):
         blocks[i]+=1
 
