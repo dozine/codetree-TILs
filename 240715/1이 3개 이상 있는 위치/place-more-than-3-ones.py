5e36eebd243746527785eff3ed0d3a6cd4b71dcb
Dozin Jang <108924060+dozine@users.noreply.github.com>
2024-07-15 00:48:17 +0900

    Title: 1이 3개 이상 있는 위치, Time: 138ms, Memory: 26MB, Status: Passed - CodeTree

--- 240715/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 240715/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -1,23 +1,29 @@
-n=in(input())
-arr=[
-    tuple(map(int,input().split()))
+# 변수 선언 및 입력
+n = int(input())
+arr = [
+    list(map(int, input().split()))
     for _ in range(n)
 ]
 
-dx=[0,1,0,-1]
-dy=[1,0,-1,0]
+dxs = [0, 1,  0, -1]
+dys = [1, 0, -1,  0]
 
-def in_range(x,y):
-    return 0 <=x and x<n and 0 <=y and y<nx
 
-def adjacent_cnt(x,y):
-    cnt=0
-    for dx,dy in zip(dxs,dys):
-        nx,ny = x+dx,y+dy
-        if in_range(nx,ny) and arr[nx][ny] ==1:
-            cnt+=1
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+
+def adjacent_cnt(x, y):
+    cnt = 0
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and arr[nx][ny] == 1:
+            cnt += 1
+    
     return cnt
 
+
+# 각 칸을 탐색합니다.
 ans = 0
 for i in range(n):
     for j in range(n):
