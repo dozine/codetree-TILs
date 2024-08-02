6c9bbe2d6aaa48684118f2e54217a11e30d26277
Dozin Jang <108924060+dozine@users.noreply.github.com>
2024-08-02 18:44:17 +0900

    Title: 체크판위에서 2, Time: 147ms, Memory: 25MB, Status: Passed - CodeTree

--- 240802/체크판위에서 2/on-the-checkboard-2.py
+++ 240802/체크판위에서 2/on-the-checkboard-2.py
@@ -1,15 +1,14 @@
-r,c=tuple(map(int,input().split()))
-arr=[
-    input().split()
-    for _ in range(r)
-]
-cnt=0
-for i in range(1,c):
-    for j in range(1,r):
-        for k in range(i+1,c-1):
-            for l in range(j+1,r-1):
+r, c = tuple(map(int, input().split()))
+arr = [input().split() for _ in range(r)]
+
+cnt = 0
+for i in range(1, r-1):  # row loop should be from 1 to r-2
+    for j in range(1, c-1):  # column loop should be from 1 to c-2
+        for k in range(i+1, r-1):  # k should start from i+1 to r-2
+            for l in range(j+1, c-1):  # l should start from j+1 to c-2
                 if arr[0][0] != arr[i][j] and \
                    arr[i][j] != arr[k][l] and \
                    arr[k][l] != arr[r-1][c-1]:
-                    cnt+=1
+                    cnt += 1
+
 print(cnt)
