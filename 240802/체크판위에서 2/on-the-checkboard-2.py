d7d9b592104bcb15df9814967e155d5be0e9d84a
Dozin Jang <108924060+dozine@users.noreply.github.com>
2024-08-02 18:22:42 +0900

    Title: 체크판위에서 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240802/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,17 @@
+r,c=tuple(map(int,input().split()))
+
+arr=[
+    input().split()
+    for _ in range(r)
+]
+
+cnt=0
+for i in range(1,c):
+    for j in range(1,r):
+        for k in range(i+1,c-1):
+            for l in range(j+1,r-1):
+                if arr[0][0] != arr[i][j] and \
+                   arr[i][j] != arr[k][l] and \
+                   arr[k][l] != arr[r-1][c-1]:
+                    cnt+=1
+print(cnt)
