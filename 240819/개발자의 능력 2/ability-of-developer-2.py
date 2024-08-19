fc1b57a57f8afb8cb3f94cc42a03a0d56e946f25
Dozin Jang <108924060+dozine@users.noreply.github.com>
2024-08-19 16:42:39 +0900

    Title: 개발자의 능력 2, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240819/개발자의 능력 2/ability-of-developer-2.py
@@ -0,0 +1,22 @@
+import sys 
+INT_MAX=sys.maxsize
+arr=list(map(int,input().split()))
+n=6
+def diff(i,j,k,l):
+    sum1=arr[i]+arr[j]
+    sum2=arr[k]+arr[l]
+    sum3=sum(arr)-sum1-sum2
+    ret = abs(sum1-sum2)
+    ret = max(ret,abs(sum2-sum3))
+    ret = max(ret,abs(sum3-sum1))
+    return ret
+
+min_diff=INT_MAX
+for i in range(n):
+    for j in range(i+1,n):
+        for k in range(n):
+            for l in range(k+1,n):
+                if k==i or k==j or l==i or l==j:
+                    continue
+                min_diff=min(min_diff,diff(i,j,k,l))
+print(min_diff)
