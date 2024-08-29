0d2e89baaefd608e724a687fd63bef2c60c248cd
Dozin Jang <108924060+dozine@users.noreply.github.com>
2024-08-29 22:03:18 +0900

    Title: 2개씩 그룹짓기, Time: 138ms, Memory: 73MB, Status: Passed - CodeTree

--- 240829/2개씩 그룹짓기/group-of-pairs.py
+++ 240829/2개씩 그룹짓기/group-of-pairs.py
@@ -1,10 +1,11 @@
-n=int(input())
-arr=list(map(int,input().split()))
-arr.sort()
+n = int(input())
+arr = list(map(int, input().split()))
 
-group_max=0
-for i in range(2*n):
-    group_sum=arr[i]+arr[-i]
-    if group_sum>group_max:
-        group_max=group_sum
-print(group_sum)
+arr.sort()  # 오름차순 정렬
+
+group_max = 0
+for i in range(n):
+    group_sum = arr[i] + arr[-(i + 1)]  # 앞쪽과 뒤쪽의 요소를 묶음
+    group_max = max(group_max, group_sum)  # 그룹의 합 중 최댓값 갱신
+
+print(group_max)
