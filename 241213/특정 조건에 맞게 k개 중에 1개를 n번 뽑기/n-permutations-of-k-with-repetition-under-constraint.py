30dcac0ba42c7052e8acaa0d013345d64a7c3631
Dozin Jang <108924060+dozine@users.noreply.github.com>
2024-12-13 00:41:23 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 229ms, Memory: 27MB, Status: Passed - CodeTree

--- 241213/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
+++ 241213/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
@@ -1,63 +1,64 @@
-# k,n=tuple(map(int,input().split()))
-# arr=[]
+k,n=tuple(map(int,input().split()))
+selected_nums=[]
 
-# def print_num():
-#     for i in arr:
-#         print(i,end=" ")
-#     print()
+def print_permutation():
+    for num in selected_nums:
+        print(num,end=" ")
+    print()
 
-# def find_num(cnt):
-#     if cnt==n+1:
-#         print_num()
-#         return 
+def find_duplicated_permutations(cnt):
+    if cnt==n:
+        print_permutation()
+        return 
 
-#     for i in range(1,n+1):
-#         arr.append(i)
-#         if arr[i]==arr[i+1] and arr[i+1]==arr[i+2]:
-#             return False
-#         find_num(cnt+1)
-#         arr.pop()
-
-# find_num(0)
-
-from itertools import product
-
-def is_valid_combination(comb):
-    """
-    주어진 조합이 조건을 만족하는지 확인합니다.
-    조건: 연속하여 같은 숫자가 3번 이상 나오면 안 된다.
-    """
-    count = 1  # 연속된 숫자의 개수
-    for i in range(1, len(comb)):
-        if comb[i] == comb[i - 1]:
-            count += 1
-            if count >= 3:
-                return False
+    for i in range(1,k+1):
+        if cnt>=2 and i == selected_nums[-1] and i==selected_nums[-2]:
+            continue
         else:
-            count = 1
-    return True
+            selected_nums.append(i)
+            find_duplicated_permutations(cnt+1)
+            selected_nums.pop()
 
-def find_combinations(K, N):
-    """
-    1 이상 K 이하의 숫자를 N번 반복하여 나올 수 있는 모든 조합을 생성합니다.
-    조건을 만족하는 조합만 반환합니다.
-    """
-    numbers = range(1, K + 1)  # 1부터 K까지의 숫자
-    all_combinations = product(numbers, repeat=N)
+find_duplicated_permutations(0)
 
-    valid_combinations = [comb for comb in all_combinations if is_valid_combination(comb)]
-    return valid_combinations
+# from itertools import product
 
-def main():
-    # 입력 받기
-    K, N = map(int, input().split())
+# def is_valid_combination(comb):
+#     """
+#     주어진 조합이 조건을 만족하는지 확인합니다.
+#     조건: 연속하여 같은 숫자가 3번 이상 나오면 안 된다.
+#     """
+#     count = 1  # 연속된 숫자의 개수
+#     for i in range(1, len(comb)):
+#         if comb[i] == comb[i - 1]:
+#             count += 1
+#             if count >= 3:
+#                 return False
+#         else:
+#             count = 1
+#     return True
 
-    # 유효한 조합 찾기
-    valid_combinations = find_combinations(K, N)
+# def find_combinations(K, N):
+#     """
+#     1 이상 K 이하의 숫자를 N번 반복하여 나올 수 있는 모든 조합을 생성합니다.
+#     조건을 만족하는 조합만 반환합니다.
+#     """
+#     numbers = range(1, K + 1)  # 1부터 K까지의 숫자
+#     all_combinations = product(numbers, repeat=N)
 
-    # 출력하기
-    for comb in valid_combinations:
-        print(" ".join(map(str, comb)))
+#     valid_combinations = [comb for comb in all_combinations if is_valid_combination(comb)]
+#     return valid_combinations
 
-if __name__ == "__main__":
-    main()
+# def main():
+#     # 입력 받기
+#     K, N = map(int, input().split())
+
+#     # 유효한 조합 찾기
+#     valid_combinations = find_combinations(K, N)
+
+#     # 출력하기
+#     for comb in valid_combinations:
+#         print(" ".join(map(str, comb)))
+
+# if __name__ == "__main__":
+#     main()
