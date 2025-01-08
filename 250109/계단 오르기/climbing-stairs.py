21272825b16c3ae0a5719878c3e58e1160d945e5
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-01-09 01:13:11 +0900

    Title: 계단 오르기, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250109/계단 오르기/climbing-stairs.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+# Write your code here!
+
+MAX_N=1000
+MOD=10007
+
+dp= [0] * (MAX_N + 1)
+
+dp[0] = 1 
+dp[1] = 0
+dp[2] = 1
+dp[3] = 1
+
+for i in range(4,n+1):
+    dp[i] = (dp[i-2] + dp[i-3]) % MOD
+print(dp[n])
