e41d16a4245d5f30172e435b4dd0db757853813a
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-01-08 23:22:50 +0900

    Title: 피보나치 수, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250108/피보나치 수/fibonacci-number.py
+++ 250108/피보나치 수/fibonacci-number.py
@@ -1,11 +1,17 @@
 N = int(input())
 
 # Write your code here!
-def fibbo(n):
-    if n<=2:
-        return 1
-    else: 
-        return fibbo(n-1)+fibbo(n-2)
 
-ans= fibbo(N)
+memo = [-1]*(N+1)
+
+def fibbo(n):
+    if memo[n] != -1:
+        return memo[n]
+    if n<=2:
+        memo[n]=1
+    else:
+        memo[n]=fibbo(n-1)+fibbo(n-2)
+    return memo[n]
+
+ans = fibbo(N)
 print(ans)
