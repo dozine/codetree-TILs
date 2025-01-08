da8f89dfa20354c122de9a5fbc4e0f5388274078
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-01-09 02:09:45 +0900

    Title: 사각형 채우기 3, Time: 55ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250109/사각형 채우기 3/rectangle-fill-3.py
+++ 250109/사각형 채우기 3/rectangle-fill-3.py
@@ -1,15 +1,16 @@
+MOD =100000007
 n = int(input())
 
 # Write your code here!
-MAX_N=1000
-dp=[0]*MAX_N
+
+dp=[0]* (n+1)
 
 dp[0]=1
 dp[1]=2
-dp[2]=7
-dp[3]=22
 
-for i in range(4,n+1):
-    dp[i] = dp[i-2]+dp[i-3]
+for i in range(2,n+1):
+    dp[i] = (dp[i-1]*2+dp[i-2]*3)%MOD
+    for j in range(i-3,-1,-1):
+        dp[i] = (dp[i]+dp[j]*2)%MOD
 
-print(dp[n]%10007)
+print(dp[n])
