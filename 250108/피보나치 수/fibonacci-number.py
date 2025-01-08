b5a62a12ac288657b53503bd5ec108b76d8eaee1
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-01-08 23:25:42 +0900

    Title: 피보나치 수, Time: 76ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250108/피보나치 수/fibonacci-number.py
+++ 250108/피보나치 수/fibonacci-number.py
@@ -2,16 +2,23 @@ N = int(input())
 
 # Write your code here!
 
-memo = [-1]*(N+1)
+# memo = [-1]*(N+1)
 
-def fibbo(n):
-    if memo[n] != -1:
-        return memo[n]
-    if n<=2:
-        memo[n]=1
-    else:
-        memo[n]=fibbo(n-1)+fibbo(n-2)
-    return memo[n]
+# def fibbo(n):
+#     if memo[n] != -1:
+#         return memo[n]
+#     if n<=2:
+#         memo[n]=1
+#     else:
+#         memo[n]=fibbo(n-1)+fibbo(n-2)
+#     return memo[n]
 
-ans = fibbo(N)
-print(ans)
+# ans = fibbo(N)
+# print(ans)
+
+dp = [0] * (N+1)
+dp[1]=1
+dp[2]=1
+for i in range(3,N+1):
+    dp[i] = dp[i-1]+dp[i-2]
+print(dp[N])
