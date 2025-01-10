70e5ff00210f2abe6b0e663da3a78b79a60cac50
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-01-10 18:21:07 +0900

    Title: 최대 감소 부분 수열, Time: 53ms, Memory: 17MB, Status: Passed - CodeTree

--- 250110/최대 감소 부분 수열/longest-decreasing-subsequence.py
+++ 250110/최대 감소 부분 수열/longest-decreasing-subsequence.py
@@ -1,14 +1,16 @@
 n = int(input())
-m = [0]+list(map(int, input().split()))
+m = list(map(int, input().split()))
 
 # Write your code here!
-dp=[1 for _ in range(n+1)]
+dp=[1 for _ in range(n)]
 
-for i in range(1,n+1):
+for i in range(n):
     for j in range(i):
         if m[i] < m[j]:
             dp[i] = max(dp[i],dp[j]+1)
 
-print(max(dp))
-
+ans=0
+for i in range(n):
+    ans = max (ans, dp[i])
+print(ans)
 
