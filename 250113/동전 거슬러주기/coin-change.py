67f69ab432c9e8db0f84085213ffcfe84ccaa868
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-01-13 15:23:35 +0900

    Title: 동전 거슬러주기, Time: 56ms, Memory: 17MB, Status: Passed - CodeTree

--- 250113/동전 거슬러주기/coin-change.py
+++ 250113/동전 거슬러주기/coin-change.py
@@ -3,7 +3,7 @@ MAX_ANS = sys.maxsize
 
 n, m = tuple(map(int, input().split()))
 
-coin = [0] + list(map(int, input().split()))
+coin = list(map(int, input().split()))
 
 dp = [MAX_ANS for _ in range(m + 1)]
 
@@ -11,7 +11,7 @@ dp[0] = 0
 
 
 for i in range(1, m + 1):
-    for j in range(1, n + 1):
+    for j in range(n):
         if i >= coin[j]:
             dp[i] = min(dp[i], dp[i - coin[j]] + 1)
 min_cnt = dp[m]
