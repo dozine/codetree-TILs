cdd2f7616d05ca57cda4b7bb0a08023fca72c231
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-01-17 23:54:48 +0900

    Title: 계단 오르기, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250117/계단 오르기/climbing-stairs.py
+++ 250117/계단 오르기/climbing-stairs.py
@@ -19,14 +19,17 @@
 
 
 n=int(input())
-
-dp=[0 for _ in range(n+1)]
+MAX_N=1000
+dp=[0 for _ in range(MAX_N+1)]
 
 dp[0]=1
 dp[1]=0
 dp[2]=1
 dp[3]=1
 
-for i in range(4,n+1):
-    dp[i]=dp[i-2]+dp[n-3]
-print(dp[n]%10007)
+if n <= 3:
+    print(dp[n] % 10007)
+else:
+    for i in range(4, n+1):
+        dp[i] = dp[i-2] + dp[i-3]
+    print(dp[n] % 10007)
