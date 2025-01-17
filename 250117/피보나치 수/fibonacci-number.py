f6ca8b4e69f48113531b5742d1838efed4b419ac
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-01-17 23:35:40 +0900

    Title: 피보나치 수, Time: 80ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250117/피보나치 수/fibonacci-number.py
+++ 250117/피보나치 수/fibonacci-number.py
@@ -34,7 +34,9 @@ dp=[0 for _ in range(n+1)]
 dp[1]=1
 dp[2]=1
 
-for i in range(3,n+1):
-    dp[i]=dp[i-1]+dp[i-2]
-
-print(dp[n])
+if n==1 or n==2:
+    print(1)
+else:
+    for i in range(3,n+1):
+        dp[i]=dp[i-1]+dp[i-2]
+    print(dp[n])
