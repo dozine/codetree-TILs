03b2f15bc126abb0723119185cff41f6b1db1b02
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-01-22 19:27:35 +0900

    Title: 행복한 수열의 개수, Time: 55ms, Memory: 17MB, Status: Passed - CodeTree

--- 250122/행복한 수열의 개수/number-of-happy-sequence.py
+++ 250122/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,29 +1,29 @@
+def is_happy_sequence(arr, m):
+    max_count = 1  # 현재까지의 최대 연속 길이
+    count = 1      # 현재 연속 길이
+    
+    for i in range(1, len(arr)):
+        if arr[i] == arr[i-1]:
+            count += 1
+            max_count = max(max_count, count)
+        else:
+            count = 1
+    
+    return max_count >= m
+
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
-cnt = 0
+happy_count = 0
 
 # 행 체크
-for i in range(n):
-    row = 1  # 각 행을 시작할 때 초기화
-    for j in range(1, n):
-        if grid[i][j] == grid[i][j-1]:
-            row += 1
-        else:
-            row = 1  # 연속되지 않으면 1로 초기화
-        if row >= m:  # m개 이상이면
-            cnt += 1
-            break  # 이미 행복한 수열임이 확인되었으므로 다음 행으로
+for row in grid:
+    if is_happy_sequence(row, m):
+        happy_count += 1
 
 # 열 체크
 for j in range(n):
-    col = 1  # 각 열을 시작할 때 초기화
-    for i in range(1, n):
-        if grid[i][j] == grid[i-1][j]:
-            col += 1
-        else:
-            col = 1  # 연속되지 않으면 1로 초기화
-        if col >= m:  # m개 이상이면
-            cnt += 1
-            break  # 이미 행복한 수열임이 확인되었으므로 다음 열로
+    column = [grid[i][j] for i in range(n)]
+    if is_happy_sequence(column, m):
+        happy_count += 1
 
-print(cnt)
+print(happy_count)
