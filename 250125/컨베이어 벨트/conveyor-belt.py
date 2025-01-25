d6d3dea5686b76bb118f8f12bef7d6534d49e68f
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-01-25 21:45:03 +0900

    Title: 컨베이어 벨트, Time: 56ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 250125/컨베이어 벨트/conveyor-belt.py
@@ -0,0 +1,22 @@
+n, t = map(int, input().split())
+u = list(map(int, input().split()))
+d = list(map(int, input().split()))
+
+# Write your code here!
+
+for i in range(t):
+    temp=u[-1]
+    for i in range(n-1,0,-1):
+        u[i]=u[i-1]
+    u[0] = d[n-1]
+
+    for i in range(n-1,0,-1):
+        d[i]=d[i-1]
+    d[0]=temp
+
+for elem in u:
+    print(elem,end=" ")
+print()
+
+for elem in d:
+    print(elem,end=" ")
