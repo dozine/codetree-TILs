3072af64f3012ba92f619b36d94b613b7de488e9
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-01-29 02:15:24 +0900

    Title: 1차원 젠가, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250129/1차원 젠가/jenga-1d.py
+++ 250129/1차원 젠가/jenga-1d.py
@@ -1,15 +1,13 @@
-n = int(input())  # 블록 개수
-blocks = [int(input()) for _ in range(n)]  # 블록 리스트
-s1, e1 = map(int, input().split())  # 첫 번째 플레이어의 범위
-s2, e2 = map(int, input().split())  # 두 번째 플레이어의 범위
+n = int(input()) 
+blocks = [int(input()) for _ in range(n)]
+s1, e1 = map(int, input().split())
+s2, e2 = map(int, input().split()) 
 
-# 첫 번째 플레이어의 범위를 제거
 remaining_blocks = blocks[1:s1] + blocks[e1:]
 
-# 두 번째 플레이어의 범위를 제거
 remaining_blocks = remaining_blocks[1:s2] + remaining_blocks[e2:]
 
-# 결과 출력
+
 print(len(remaining_blocks))
 for i in remaining_blocks:
     print(i)
