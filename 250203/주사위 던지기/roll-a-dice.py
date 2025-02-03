862ca607085ee954324f10d7fcbc4002ae1ccc1d
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-02-03 20:53:13 +0900

    Title: 주사위 던지기, Time: 71ms, Memory: 18MB, Status: Passed - CodeTree

--- 250203/주사위 던지기/roll-a-dice.py
+++ 250203/주사위 던지기/roll-a-dice.py
@@ -16,15 +16,17 @@ def solution():
     dx = [0, 0, -1, 1]
     dy = [1, -1, 0, 0]
     
-    # 현재 위치에 주사위 바닥면 값 표시
     grid[x][y] = dice[1]  # 아래면
     
     for cmd in commands:
-        # 이동 방향 결정
-        if cmd == 'R': dir = 0
-        elif cmd == 'L': dir = 1
-        elif cmd == 'U': dir = 2
-        else: dir = 3
+        if cmd == 'R': 
+            dir = 0
+        elif cmd == 'L': 
+            dir = 1
+        elif cmd == 'U': 
+            dir = 2
+        else: 
+            dir = 3
         
         # 다음 위치 계산
         nx, ny = x + dx[dir], y + dy[dir]
@@ -34,19 +36,16 @@ def solution():
             continue
         
         # 주사위 굴리기
-        temp = dice[:]
-        if cmd == 'R':  # 동쪽
+        if cmd == 'R': 
             dice[0], dice[1], dice[4], dice[5] = dice[4], dice[5], dice[1], dice[0]
-        elif cmd == 'L':  # 서쪽
+        elif cmd == 'L': 
             dice[0], dice[1], dice[4], dice[5] = dice[5], dice[4], dice[0], dice[1]
-        elif cmd == 'U':  # 북쪽
+        elif cmd == 'U': 
             dice[0], dice[1], dice[2], dice[3] = dice[2], dice[3], dice[1], dice[0]
-        else:  # 남쪽
+        else: 
             dice[0], dice[1], dice[2], dice[3] = dice[3], dice[2], dice[0], dice[1]
         
-        # 위치 업데이트
         x, y = nx, ny
-        # 현재 위치에 주사위 바닥면 값 표시
         grid[x][y] = dice[1]
     
     return sum(sum(row) for row in grid)
