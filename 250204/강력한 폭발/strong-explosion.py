895fb169bb28dc909a0dd44978ad327799e1b1e7
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-02-04 17:58:38 +0900

    Title: 강력한 폭발, Time: 66ms, Memory: 17MB, Status: Passed - CodeTree

--- 250204/강력한 폭발/strong-explosion.py
+++ 250204/강력한 폭발/strong-explosion.py
@@ -1,85 +1,50 @@
-# 변수 선언 및 입력:
 n = int(input())
-bomb_type = [
-    [0 for _ in range(n)]
-    for _ in range(n)
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# 폭탄이 놓일 수 있는 위치를 저장
+bomb_pos = [(i, j) for i in range(n) for j in range(n) if grid[i][j] == 1]
+
+# 폭탄 타입별 영향 범위
+bomb_patterns = [
+    [],
+    [[-2, 0], [-1, 0], [0, 0], [1, 0], [2, 0]],  # 1번 폭탄 (세로 직선)
+    [[-1, 0], [1, 0], [0, 0], [0, -1], [0, 1]],  # 2번 폭탄 (십자 모양)
+    [[-1, -1], [-1, 1], [0, 0], [1, -1], [1, 1]] # 3번 폭탄 (대각선)
 ]
-bombed = [
-    [False for _ in range(n)]
-    for _ in range(n)
-]
 
-ans = 0
-
-bomb_pos = list()
-
+# 폭탄이 터진 위치를 저장할 배열 (초기화: 0)
+is_bombed = [[0] * n for _ in range(n)]
 
 def in_range(x, y):
-    return 0 <= x and x < n and 0 <= y and y < n
+    return 0 <= x < n and 0 <= y < n
 
-
-def bomb(x, y, b_type):
-    # 폭탄 종류마다 터질 위치를 미리 정의합니다.
-    bomb_shapes = [
-        [],
-        [[-2, 0], [-1, 0], [0, 0], [1, 0], [2, 0]],
-        [[-1, 0], [1, 0], [0, 0], [0, -1], [0, 1]],
-        [[-1, -1], [-1, 1], [0, 0], [1, -1], [1, 1]]
-    ]
-    
-    # 격자 내 칸에 대해서만 영역을 표시합니다.
-    for i in range(5):
-        dx, dy = bomb_shapes[b_type][i];
+def apply_bomb(x, y, bomb_type, delta):
+    """
+    폭탄을 배치하거나 원상 복구 (delta=+1: 배치, -1: 원상 복구)
+    """
+    for dx, dy in bomb_patterns[bomb_type]:
         nx, ny = x + dx, y + dy
         if in_range(nx, ny):
-            bombed[nx][ny] = True
+            is_bombed[nx][ny] += delta  # 영향 반영 (중복 처리)
 
-            
-def calc():
-    # Step1. 폭탄이 터진 위치를 표시하는 배열을
-    # 초기화합니다.
-    for i in range(n):
-        for j in range(n):
-            bombed[i][j] = False
-            
-    # Step2. 각 폭탄의 타입에 따라 
-    # 초토화 되는 영역을 표시합니다.
-    for i in range(n):
-        for j in range(n):
-            if bomb_type[i][j]:
-                bomb(i, j, bomb_type[i][j])
-	
-    # Step3. 초토화된 영역의 수를 구합니다.
-    cnt = 0
-    for i in range(n):
-        for j in range(n):
-            if bombed[i][j]:
-                cnt += 1
-    
-    return cnt
+def count_bombed():
+    """초토화된 영역 수 계산"""
+    return sum(1 for i in range(n) for j in range(n) if is_bombed[i][j] > 0)
 
+def backtracking(cnt, max_val):
+    """백트래킹으로 최대 초토화 영역 찾기"""
+    if cnt == len(bomb_pos):  # 모든 폭탄을 배치했을 때
+        return max(max_val, count_bombed())
 
-def find_max_area(cnt):
-    global ans
-    
-    if cnt == len(bomb_pos):
-        ans = max(ans, calc())
-        return
+    x, y = bomb_pos[cnt]  # 현재 폭탄 배치 위치
 
-    for i in range(1, 4):
-        x, y = bomb_pos[cnt]
-        
-        bomb_type[x][y] = i
-        find_max_area(cnt + 1)
-        bomb_type[x][y] = 0
+    for bomb_type in range(1, 4):  # 폭탄 타입 (1~3번)
+        apply_bomb(x, y, bomb_type, 1)  # 폭탄 배치
+        max_val = backtracking(cnt + 1, max_val)
+        apply_bomb(x, y, bomb_type, -1)  # 원상 복구
 
+    return max_val
 
-for i in range(n):
-    given_row = list(map(int, input().split()))
-    for j, bomb_place in enumerate(given_row):
-        if bomb_place:
-            bomb_pos.append((i, j))
-
-find_max_area(0)
-
-print(ans)
+# 백트래킹 실행
+result = backtracking(0, 0)
+print(result)
