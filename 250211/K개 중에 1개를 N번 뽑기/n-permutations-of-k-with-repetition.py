15e5de06162d57c3af67b5518bd7e0a9fd806b08
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-02-11 15:50:51 +0900

    Title: K개 중에 1개를 N번 뽑기, Time: 248ms, Memory: 27MB, Status: Passed - CodeTree

---
+++ 250211/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.py
@@ -0,0 +1,24 @@
+K, N = map(int, input().split())
+
+# Write your code here!
+selected_nums=[]
+
+def print_permutation():
+    for num in selected_nums:
+        print(num, end=" ")
+    print()
+
+def find_permutation(cnt):
+    if cnt==N:
+        print_permutation()
+        return 
+    for i in range(1,K+1):
+        selected_nums.append(i)
+        find_permutation(cnt+1)
+        selected_nums.pop()
+
+find_permutation(0)
+
+
+
+
