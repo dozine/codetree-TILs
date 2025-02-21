bcd8f103570e620ea90b50f12b6fa960349ffd28
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-02-22 01:06:09 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 85ms, Memory: 19MB, Status: Passed - CodeTree

---
+++ 250222/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+n, m = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(n)]
+
+visited = [
+   [False for _ in range(m)]
+   for _ in range(n)
+]
+q=deque()
+
+def in_range(x,y):
+    return 0<=x and x<n and 0<=y and y<m
+
+def can_go(x,y):
+    return in_range(x,y) and a[x][y] and not visited[x][y]
+
+
+def bfs():
+    while q: 
+        x,y=q.popleft()
+        dxs,dys=[0,1,0,-1],[1,0,-1,0]
+        for dx,dy in zip(dxs,dys):
+            nx,ny=dx+x, dy+y
+            if can_go(nx,ny):
+                q.append((nx,ny))
+                visited[nx][ny]=True
+
+q.append((0,0))
+visited[0][0]=True
+bfs()
+answer = 1 if visited[n-1][m-1] else 0 
+print(answer)
