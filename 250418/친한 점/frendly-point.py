c1d99ddc2f7cc113e57e68cc93eca5d22e4f9b4a
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-04-18 01:44:21 +0900

    Title: 친한 점, Time: 1096ms, Memory: 57MB, Status: Time Limit Exceed - Codetree

--- 250418/친한 점/frendly-point.py
+++ 250418/친한 점/frendly-point.py
@@ -1,20 +1,26 @@
 from sortedcontainers import SortedSet
-n, m = map(int, input().split())
 
-# Store points as list of tuples
-points = [tuple(map(int, input().split())) for _ in range(n)]
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+points = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
+queries = [
+    tuple(map(int, input().split()))
+    for _ in range(m)
+]
 
-# Store queries as list of tuples
-queries = [tuple(map(int, input().split())) for _ in range(m)]
+# treeset에 점들을 넣어줍니다.
+s = SortedSet(points)
 
-# Please write your code here.
-
-s=SortedSet(points)
-
+# 질의마다 조건에 맞는 점을 찾아줍니다.
 for target in queries:
     idx = s.bisect_left(target)
+    # 그러한 점이 없다면 -1을 출력합니다.
     if idx == len(s):
-        print(-1,-1)
+        print(-1, -1)
+    # 존재한다면, 그 지점의 값을 출력합니다.
     else:
-        x,y = s[idx]
-        print(x,y)
+        x, y = s[idx]
+        print(x, y)
