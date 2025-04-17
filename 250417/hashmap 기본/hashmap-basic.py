8f150a6c15c0224d8946d5f31dfb41997b58df82
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-04-17 19:19:28 +0900

    Title: hashmap 기본, Time: 238ms, Memory: 24MB, Status: Passed - Codetree

--- 250417/hashmap 기본/hashmap-basic.py
+++ 250417/hashmap 기본/hashmap-basic.py
@@ -2,18 +2,34 @@
 n = int(input())
 d = dict()
 
-for _ in range(n):
-    command = input()
+# for _ in range(n):
+#     command = input()
 
-    if command.startswith("add"):
-        _, k, v = tuple(command.split())
-        k, v = int(k), int(v)
-        d[k] = v
-    elif command.startswith("remove"):
-        k = int(command.split()[1])
+#     if command.startswith("add"):
+#         _, k, v = tuple(command.split())
+#         k, v = int(k), int(v)
+#         d[k] = v
+#     elif command.startswith("remove"):
+#         k = int(command.split()[1])
+#         d.pop(k)
+#     else:
+#         k = int(command.split()[1])
+#         if k not in d:
+#             print("None")
+#         else:
+#             print(d[k])
+
+for _ in range(n):
+    command=list(input().split())
+    if command[0]=="add":
+        k=int(command[1])
+        v=int(command[2])
+        d[k]=v
+    elif command[0]=="remove":
+        k=int(command[1])
         d.pop(k)
     else:
-        k = int(command.split()[1])
+        k=int(command[1])
         if k not in d:
             print("None")
         else:
