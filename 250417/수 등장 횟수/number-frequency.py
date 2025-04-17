00a104535ad57aa1a55b022d0c378fa18cca3d21
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-04-17 19:30:09 +0900

    Title: 수 등장 횟수, Time: 108ms, Memory: 29MB, Status: Passed - Codetree

---
+++ 250417/수 등장 횟수/number-frequency.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+freq=dict()
+
+for elem in arr:
+    if elem not in freq:
+        freq[elem] = 1
+    else:
+        freq[elem] += 1
+queries = list(map(int,input().split()))
+for num in queries:
+    if num not in freq:
+        print(0,end=" ")
+    else:
+        print(freq[num],end=" ")
