a7bacacca4156d5a7317e544d642028225633de2
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-04-28 17:33:16 +0900

    Title: 정수 N개의 합 2, Time: 1101ms, Memory: 24MB, Status: Time Limit Exceed - Codetree

--- 250428/정수 N개의 합 2/sum-of-n-integers-2.py
+++ 250428/정수 N개의 합 2/sum-of-n-integers-2.py
@@ -2,9 +2,9 @@ n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 ps=[0]*(n+1)
 # Please write your code here.
-for i in range(n-3):
-    for k in range(i,i+3):
-        ps[i]+=k
+for i in range(n-k+1):
+    for j in range(i,i+k):
+        ps[i]+=arr[j]
 
 answer=max(ps)
 print(answer) 
