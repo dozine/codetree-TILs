b3e77541f42673e59f0cb5643ca187952c1178fc
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-04-29 17:40:41 +0900

    Title: 점 개수 세기 3, Time: 257ms, Memory: 30MB, Status: Passed - Codetree

---
+++ 250429/점 개수 세기 3/count-number-of-points-3.py
@@ -0,0 +1,18 @@
+from sortedcontainers import SortedSet
+
+n, q = map(int, input().split())
+points = list(map(int, input().split()))
+queries = [tuple(map(int, input().split())) for _ in range(q)]
+
+# Please write your code here.
+nums=SortedSet(points)
+
+mapper=dict()
+cnt=1
+for num in nums:
+    mapper[num]=cnt
+    cnt+=1
+
+for a,b in queries:
+    na,nb=mapper[a],mapper[b]
+    print(nb-na+1)
