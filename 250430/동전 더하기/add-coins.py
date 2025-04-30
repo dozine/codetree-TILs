327de671db13b607b99f8d89b72926e87ebc6ea6
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-04-30 19:07:24 +0900

    Title: 동전 더하기, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250430/동전 더하기/add-coins.py
+++ 250430/동전 더하기/add-coins.py
@@ -2,9 +2,9 @@ n, k = map(int, input().split())
 coins = [int(input()) for _ in range(n)]
 
 # Please write your code here.
-
+coins.sort(reverse=True)
 ans=0
-for coin in coins[::-1]:
+for coin in coins:
     ans+=k//coin
     k%=coin
 
