eb007130aed831b2e296cdbef644dec3e36f6975
Dozin Jang <108924060+dozine@users.noreply.github.com>
2025-05-05 17:05:52 +0900

    Title: 가장 오래 걸리는 학생 2, Time: 77ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250505/가장 오래 걸리는 학생 2/longest-student-2.py
@@ -0,0 +1,38 @@
+import sys 
+INT_MAX=sys.maxsize
+
+import heapq
+
+n,m=tuple(map(int,input().split()))
+graph=[
+    []for _ in range(n+1)
+]
+dist=[INT_MAX]*(n+1)
+pq=[]
+
+for _ in range(n+1):
+    x,y,z=tuple(map(int,input().split()))
+    graph[x].append((y,z))
+    graph[y].append((x,z))
+
+dist[n]=0
+heapq.heappush(pq,(0,n))
+
+while pq:
+    min_dist,min_index=heapq.heappop(pq)
+    
+    if min_dist!=dist[min_index]:
+        continue
+    for target_index,target_dist in graph[min_index]:
+        new_dist = dist[min_index] + target_dist
+        if dist[target_index] > new_dist:
+            dist[target_index] = new_dist
+            heapq.heappush(pq, (new_dist, target_index))
+
+max_time = 0
+for i in range(1, n):
+    if dist[i] == INT_MAX:
+        continue 
+    max_time = max(max_time, dist[i])
+
+print(max_time)
