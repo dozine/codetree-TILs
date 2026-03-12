0cb821fa972227c18a493548ca1d0bef00f64aaf
Dojin Jang <108924060+dozine@users.noreply.github.com>
2026-03-12 22:59:54 +0900

    Title: 십진수로 변환하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260312/십진수로 변환하기/convert-to-decimal.py
+++ 260312/십진수로 변환하기/convert-to-decimal.py
@@ -1,8 +1,11 @@
-binary = list(map(int,input().split()))
+# 변수 선언 및 입력
+binary = list(map(int, list(input())))
 length = len(binary)
 
+# 십진수로 변환합니다.
 num = 0
 for i in range(length):
     num = num * 2 + binary[i]
 
+# 출력
 print(num)
