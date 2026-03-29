ef26c635842b37c5e6a8b026b9215c1f9cd7215d
Dojin Jang <108924060+dozine@users.noreply.github.com>
2026-03-29 14:53:48 +0900

    Title: 연속되는 수 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260329/연속되는 수 3/continuous-number3.py
+++ 260329/연속되는 수 3/continuous-number3.py
@@ -2,8 +2,8 @@ n = int(input())
 arr = [int(input()) for _ in range(n)]
 
 # Please write your code here.
-cnt = 0 
-ans =0
+cnt = 1
+ans = 1
 
 for i in range(1,len(arr)):
     if arr[i-1]*arr[i] > 0:
