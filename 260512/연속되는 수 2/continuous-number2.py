f365dfff0bcb85b332a4a609e593d1309d52d4ec
Dojin Jang <108924060+dozine@users.noreply.github.com>
2026-05-12 23:58:55 +0900

    [Passed] 연속되는 수 2 (64ms, 16MB)

---
+++ 260512/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,19 @@
+n = int(input())
+if n == 0:
+    print(0)
+else:
+    arr = [int(input()) for _ in range(n)]
+    cnt = 1
+    maxx = 1
+    for i in range(1, n):
+        if arr[i] == arr[i-1]:
+            cnt += 1
+        else:
+            cnt = 1
+        
+        if cnt > maxx:
+            maxx = cnt
+
+    print(maxx)
+
+
