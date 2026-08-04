6150fa5f448a00bdd50dcb8e4076778f87a5ff82
Dojin Jang <108924060+dozine@users.noreply.github.com>
2026-08-04 23:29:11 +0900

    [Passed] 괄호 쌍 만들어주기 3 (52ms, 16MB)

---
+++ trail2/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -0,0 +1,11 @@
+A = input()
+
+# Please write your code here.
+
+ans = 0
+for i in range(len(A)):
+    for j in range(i+1,len(A)):
+        if A[i]=='(' and A[j]==")":
+            ans +=1
+
+print(ans)
