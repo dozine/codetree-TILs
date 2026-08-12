b124383d649c7d4aa1aa8437e50dfd0c38029dcd
Dojin Jang <108924060+dozine@users.noreply.github.com>
2026-08-13 00:40:25 +0900

    [Passed] 특정 구간의 원소 평균값 (56ms, 16MB)

--- trail2/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ trail2/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -17,20 +17,36 @@
 #             cnt+=1
 # print(cnt)
 
+# n = int(input())
+# arr = list(map(int, input().split()))
+
+# cnt = 0
+
+# for i in range(n):
+#     for j in range(i, n):
+#         total = sum(arr[i:j+1])
+#         length = j - i + 1
+#         if (total/length) in arr[i:j+1]:
+#             cnt += 1
+
+# print(cnt)
+
 n = int(input())
 arr = list(map(int, input().split()))
 
 cnt = 0
 
 for i in range(n):
+    sum_interval = 0
+    nums = set()
     for j in range(i, n):
-        total = sum(arr[i:j+1])
+        sum_interval += arr[j]
+        nums.add(arr[j])
         length = j - i + 1
 
-        if total % length == 0:
-            avg = total // length
-
-            if avg in arr[i:j+1]:
+        if sum_interval % length == 0:
+            avg = sum_interval // length
+            if avg in nums:
                 cnt += 1
 
 print(cnt)
