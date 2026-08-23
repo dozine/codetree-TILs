6aecc4995c28188acd8b4e8025104a2a3fb6f005
Dojin Jang <108924060+dozine@users.noreply.github.com>
2026-08-23 21:36:17 +0900

    [Passed] 운행 되고 있는 시간 (96ms, 36MB)

---
+++ trail2/운행 되고 있는 시간/hours-in-service.py
@@ -0,0 +1,45 @@
+# n=int(input())
+# time=[
+#     tuple(map(int,input().split()))
+#     for _ in range(n)
+# ]
+
+# ans=0
+# for i in range(n):
+#     working=[0]*1000
+#     for j,(x,y) in enumerate(time):
+#         if j==i:
+#             continue
+#         for k in range(x,y):
+#             working[k]+=1
+#     t=0
+#     for j in range(1,1000):
+#         if working[j]>0:
+#             t+=1
+#     ans=max(ans,t)
+# print(ans)
+
+
+n = int(input())
+
+time = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
+
+max_ans = -1
+
+for i in range(n):
+    working = [0] * 1000
+
+    for j, (x, y) in enumerate(time):
+        if i == j:
+            continue
+
+        for t in range(x, y):
+            working[t] = 1
+
+    ans = sum(working)
+    max_ans = max(ans, max_ans)
+
+print(max_ans)
